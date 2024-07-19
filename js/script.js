afc57a357d2e291184cc351c085b609b08966cda
JBlemy <jblemy2002@gmail.com>
2024-07-18 22:22:45 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function affichermenu()
+{
+    if (document.getElementById("menu").style.display == "block")
+    {
+        document.getElementById("menu").style.display="none";
+    }
+    else
+    {
+        document.getElementById("menu").style.display="block";
+    }
+}
